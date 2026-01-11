d4e1f4e612f451e7af757188b6d9904992945b5b
Aleksander Michnowicz <aleksander@macbookair.home>
2026-01-11 15:29:37 +0100

    header scss

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,31 +1,57 @@
+'use client'
+
 import Link from "next/link";
+import Image from 'next/image';
+import {useEffect, useState} from "react";
+import "./Header.scss";
+import logo_horizontal from "@/public/img/aquapro_logo_horizontal.svg";
+import burger from "@/public/img/burger-bar.png"
 
 export default function Header() {
+  const [nav, setNav] = useState("inactive")
+  const [burger, setBurger] = useState("")
+  
+  function toggleBurger() {
+    if (burger === "") {
+      setBurger("cross")
+      setNav("active")
+    } else {
+      setBurger("")
+      setNav("inactive")
+    }
+  }
+
   return (
     <header className="topbar">
       <div className="container">
         <div className="navWrap">
           <Link href="/" className="logo">
-            AquaPro-Détect
+            <Image src={logo_horizontal} alt="AquaPro-Détect" className="icon"/>
           </Link>
 
-          <nav className="nav">
-            <Link className="pill" href="/services">
+          <button className={"burger " + burger} onClick={toggleBurger}>
+              <div className="bar1"></div>
+              <div className="bar2"></div>
+              <div className="bar3"></div>
+          </button>
+
+          <nav className={"nav " + nav}>
+            <Link className="pill" href="/services" onClick={toggleBurger}>
               Services
             </Link>
-            <Link className="pill" href="/problemes">
+            <Link className="pill" href="/problemes" onClick={toggleBurger}>
               Problèmes
             </Link>
-            <Link className="pill" href="/assurances-rapports">
+            <Link className="pill" href="/assurances-rapports" onClick={toggleBurger}>
               Assurances & Rapports
             </Link>
-            <Link className="pill" href="/a-propos">
+            <Link className="pill" href="/a-propos" onClick={toggleBurger}>
               À propos
             </Link>
-            <Link className="pill" href="/contact">
+            <Link className="pill" href="/contact" onClick={toggleBurger}>
               Contact
             </Link>
-            <Link className="pill pillPrimary" href="/urgence-24-7">
+            <Link className="pill pillPrimary" href="/urgence-24-7" onClick={toggleBurger}>
               Urgence 24/7
             </Link>
           </nav>
